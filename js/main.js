6e9b12a4798c8e8b7dd69f7eb5e32f3518af69f8
Nurik <nurik.csgo@gmail.com>
2021-12-10 07:47:32 +0600

    html,css,js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.sliders').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: '',
+        prevArrow: ''
+    });
+});
